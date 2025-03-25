46e6dedec277d94709dee284263f6080c650321d
Gabriel Silva <gabriel.silva@areco.net.br>
2025-03-25 12:09:44 -0300

    FT: Added Command1 test

--- test/command_test.dart
+++ test/command_test.dart
@@ -37,12 +37,54 @@ void main() {
       await command0.execute();
 
       //* Assert
-      expect(command0.completed, false);  //? Did not ended the action
+      expect(command0.completed, false); //? Did not ended the action
       expect(command0.error, true);
       expect(command0.running, false);
 
       expect(command0.result, isNotNull);
       expect(command0.result?.asError.error, isA<Exception>());
+    });
+
+    test('Should test Command1 returning Ok', () async {
+      //? Arrange
+      final command1 = Command1<String, String>(getOKResult1);
+
+      expect(command1.completed, false);
+      expect(command1.error, false);
+      expect(command1.running, false);
+      expect(command1.result, isNull);
+
+      //! Act
+      await command1.execute('Input parameter');
+
+      //* Assert
+      expect(command1.completed, true);
+      expect(command1.error, false);
+      expect(command1.running, false);
+
+      expect(command1.result, isNotNull);
+      expect((command1.result as Ok).value, 'Input parameter');
+    });
+
+    test('Should test Command1 returning Error', () async {
+      //? Arrange
+      final command1 = Command1<bool, String>(getErrorResult1);
+
+      expect(command1.completed, false);
+      expect(command1.error, false);
+      expect(command1.running, false);
+      expect(command1.result, isNull);
+
+      //! Act
+      await command1.execute('Input parameter');
+
+      //* Assert
+      expect(command1.completed, false);
+      expect(command1.error, false);
+      expect(command1.running, false);
+
+      expect(command1.result, isNotNull);
+      expect((command1.result as Error).error, isA<Exception>());
     });
   });
 }
@@ -53,6 +95,16 @@ Future<Result<String>> getOKResult() async {
 }
 
 Future<Result<bool>> getErrorResult() async {
+  await Future.delayed(const Duration(seconds: 1));
+  return Result.error(Exception('Something went wrong...'));
+}
+
+Future<Result<String>> getOKResult1(String param) async {
+  await Future.delayed(const Duration(seconds: 1));
+  return Result.ok(param);
+}
+
+Future<Result<bool>> getErrorResult1(String param) async {
   await Future.delayed(const Duration(seconds: 1));
   return Result.error(Exception('Something went wrong...'));
 }
