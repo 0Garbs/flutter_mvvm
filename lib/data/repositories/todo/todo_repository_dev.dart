b4e3919ff0f83ee0b7933ba6b88c474dec18efb0
Gabriel Silva <gabriel.silva@areco.net.br>
2025-03-25 18:26:13 -0300

    FT: Implementing the Interface with local data

---
+++ lib/data/repositories/todo/todo_repository_dev.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_mvvm/core/result/result.dart';
+import 'package:flutter_mvvm/data/repositories/todo/todo_repository.dart';
+import 'package:flutter_mvvm/domain/models/todo.dart';
+
+class TodoRepositoryDev implements TodoRepository {
+  final _todos = <Todo>[];
+
+  @override
+  Future<Result<Todo>> add(String name) async {
+    final lastTodoIndex = _todos.length;
+
+    final newTodo = Todo(id: lastTodoIndex, name: name);
+
+    _todos.add(newTodo);
+
+    return Result.ok(newTodo);
+  }
+
+  @override
+  Future<Result<List<Todo>>> get() async {
+    return Result.ok(_todos);
+  }
+
+  @override
+  Future<Result<void>> remove(Todo todo) async {
+    if (_todos.contains(todo)) {
+      _todos.remove(todo);
+    }
+
+    return Result.ok(null);
+  }
+}
