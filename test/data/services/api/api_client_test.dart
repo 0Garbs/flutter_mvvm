410cb6ff07e8215afcf4c4bb938a9158c7a5878c
Gabriel Silva <gabriel.silva@areco.net.br>
2025-03-26 16:12:18 -0300

    FT: Added postTodo test

--- test/data/services/api/api_client_test.dart
+++ test/data/services/api/api_client_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_mvvm/data/services/api_client.dart';
+import 'package:flutter_mvvm/data/services/api/api_client.dart';
 import 'package:flutter_mvvm/domain/models/todo.dart';
 import 'package:flutter_mvvm/utils/result/result.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,10 +12,20 @@ void main() {
 
   group('Should teste [ApiClient]', () {
     test('Should return Result Ok when getTodos()', () async {
-      client = ApiClient();
+      //? Arrange
+      //! Act
       final result = await client.getTodos();
 
+      //* Assert
       expect(result.asOk.value, isA<List<Todo>>());
+    });
+
+    test('Should create a Todo calling postTodo', () async {
+      //? Arrange
+      //! Act
+      final result = await client.postTodo(Todo(id: 0, name: 'Todo 1'));
+
+      expect((result as Ok).value, isA<void>());
     });
   });
 }
