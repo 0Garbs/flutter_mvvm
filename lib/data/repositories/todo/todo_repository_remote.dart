c84576d541047cf74db68524f0c95c9a6dce6913
Gabriel Silva <gabriel.silva@areco.net.br>
2025-03-27 10:55:04 -0300

    FT: Added remote repository for real use

---
+++ lib/data/repositories/todo/todo_repository_remote.dart
@@ -0,0 +1,60 @@
+import 'package:flutter_mvvm/data/repositories/todo/todo_repository.dart';
+import 'package:flutter_mvvm/data/services/api/api_client.dart';
+import 'package:flutter_mvvm/data/services/api/models/todo/todo_api_model.dart';
+import 'package:flutter_mvvm/domain/models/todo.dart';
+import 'package:flutter_mvvm/utils/result/result.dart';
+
+class TodoRepositoryRemote implements TodoRepository {
+  final ApiClient _apiClient;
+
+  const TodoRepositoryRemote({required ApiClient apiClient})
+      : _apiClient = apiClient;
+
+  @override
+  Future<Result<Todo>> add(String name) async {
+    try {
+      final result = await _apiClient.postTodo(CreateTodoApiModel(name: name));
+
+      switch (result) {
+        case Ok<Todo>():
+          return result;
+        default:
+          return result;
+      }
+    } on Exception catch (error) {
+      return Result.error(error);
+    }
+  }
+
+  @override
+  Future<Result<List<Todo>>> get() async {
+    final result = await _apiClient.getTodos();
+
+    try {
+      switch (result) {
+        case Ok<List<Todo>>():
+          return result;
+        default:
+          return result;
+      }
+    } on Exception catch (error) {
+      return Result.error(error);
+    }
+  }
+
+  @override
+  Future<Result<void>> remove(Todo todo) async {
+    try {
+      final result = await _apiClient.deleteTodo(todo);
+
+      switch (result) {
+        case Ok<void>():
+          return result;
+        default:
+          return result;
+      }
+    } on Exception catch (error) {
+      return Result.error(error);
+    }
+  }
+}
