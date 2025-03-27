e558b518bc87d9ca55362965183cfe2063b3a59b
Gabriel Silva <gabriel.silva@areco.net.br>
2025-03-27 11:57:20 -0300

    FT: Added GoRouter to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mvvm/data/repositories/todo/todo_repository_remote.dart';
-import 'package:flutter_mvvm/data/services/api/api_client.dart';
-import 'package:flutter_mvvm/ui/todo/viewmodels/todo_viewmodel.dart';
-import 'package:flutter_mvvm/ui/todo/widgets/todo_screen.dart';
+import 'package:flutter_mvvm/routing/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: false,
       ),
-      home: TodoScreen(
-        todoViewmodel: TodoViewmodel(todosRepository: TodoRepositoryRemote(apiClient: ApiClient(host: '10.0.1.80'))),
-      ),
+      routerConfig: routerConfig(),
     );
   }
 }
